a09bbd4d8f24b7e986ebb634b884fb9dd347fd4f
ZsoltJuhasz <juhasz.zsolt98@gmail.com>
2021-12-06 09:10:43 +0100

    Init

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [base, setBase] = useState();
+  const [height, setHeight] = useState();
+  const [area, setArea] = useState();
+
+  function calcArea() {
+    let result = base * height / 2; 
+    setArea(result);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Triangle</Text>
+
+      <Text>Alap</Text>
+      <TextInput
+      style={styles.input} 
+      placeholder='Alap'
+      onChangeText={ base  => setBase(base) } />
+
+      <Text>Magasság</Text>
+      <TextInput
+      style={styles.input} 
+      placeholder='Magasság'
+      onChangeText={ height  => setHeight(height) } />
+
+      <Button 
+        title="Számít"
+        onPress={calcArea}
+      />
+
+      <Text>Terület</Text>
+      <Text>{area}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +48,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    width: '80%',
+    marginTop: 8,
+    marginBottom: 8,
+  }
 });
